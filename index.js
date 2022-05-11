d381b2fe5750b16938a8313b30f4c33641b1430e
denissemb <denissemb@gmail.com>
2022-05-11 21:46:49 +0200

    Config cors, express.json, babel

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express=require('express');
-const morgan=require('morgan');
+import express from 'express';
+//const express=require('express');
+import morgan from 'morgan';
+//const morgan=require('morgan');
+import cors from 'cors';
+//const cors=require('cors');
+import path from 'path';
 
 const app=express();
 app.use(morgan('dev'));
+app.use(cors());
+
+app.use(express.json());
+//Para habiltar al backend las peticiones dle json con el método POST
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join (__dirname,'public')));
 
 app.set('port',process.env.PORT || 3000);
 
 app.listen(app.get('port'),()=>{
 console.log('server on port' +app.get('port'));
+//console.log(path.join (__dirname,'public'));
 
 });
+
+//Se han configurado los archivos estáticos (video 9)
